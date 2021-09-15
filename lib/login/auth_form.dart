530ede46a48f9195aa307e8db78e398c530be259
Akshil <shahakshil2001@gmail.com>
2021-09-15 23:27:17 +0530

    Ui added

--- lib/login/auth_form.dart
+++ lib/login/auth_form.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:io';
 
 class AuthForm extends StatefulWidget {
   AuthForm(
@@ -43,90 +42,204 @@ class _AuthFormState extends State<AuthForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        margin: EdgeInsets.all(20),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextFormField(
-                    key: ValueKey('email'),
-                    validator: (value) {
-                      if (value.isEmpty || !value.contains('@')) {
-                        return 'Please enter a valid email address';
-                      }
-                      return null;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      labelText: 'Email Address',
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(60)),
+              child: Image.asset(
+                "assets/login_pic2.png",
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.45,
+                fit: BoxFit.fill,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.1),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Email',
+                      style: TextStyle(
+                        color: Color(0xff3F70B5),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                      ),
                     ),
-                    onSaved: (value) {
-                      _userEmail = value;
-                    },
-                  ),
-                  if (!_isLogin)
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.02,
+                    ),
                     TextFormField(
-                      key: ValueKey('username'),
+                      key: ValueKey('email'),
                       validator: (value) {
-                        if (value.isEmpty || value.length < 4) {
-                          return 'Please enter at least 4 characters';
+                        if (value.isEmpty || !value.contains('@')) {
+                          return 'Please enter a valid email address';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 2),
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Colors.black,
+                            width: 2.0,
+                          ),
+                        ),
+                        labelText: '  Enter Email Address',
+                      ),
+                      onSaved: (value) {
+                        _userEmail = value;
+                      },
+                    ),
+                    if (!_isLogin)
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.02,
+                      ),
+                    if (!_isLogin)
+                      Text(
+                        'Username',
+                        style: TextStyle(
+                          color: Color(0xff3F70B5),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                        ),
+                      ),
+                    if (!_isLogin)
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.02,
+                      ),
+                    if (!_isLogin)
+                      TextFormField(
+                        key: ValueKey('username'),
+                        validator: (value) {
+                          if (value.isEmpty || value.length < 4) {
+                            return 'Please enter at least 4 characters';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(vertical: 2),
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.black, width: 2.0),
+                          ),
+                          labelText: '   Enter Username',
+                        ),
+                        onSaved: (value) {
+                          _userName = value;
+                        },
+                      ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.025,
+                    ),
+                    Text(
+                      'Password',
+                      style: TextStyle(
+                        color: Color(0xff3F70B5),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.02,
+                    ),
+                    TextFormField(
+                      key: ValueKey('password'),
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 8) {
+                          return 'Password must be at least 8 character long.';
                         }
                         return null;
                       },
                       decoration: InputDecoration(
-                        labelText: 'Username',
+                        contentPadding: EdgeInsets.symmetric(vertical: 2),
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.black, width: 2.0),
+                        ),
+                        labelText: '   Enter Password',
                       ),
+                      obscureText: true,
                       onSaved: (value) {
-                        _userName = value;
+                        _userPassword = value;
                       },
                     ),
-                  TextFormField(
-                    key: ValueKey('password'),
-                    validator: (value) {
-                      if (value.isEmpty || value.length < 8) {
-                        return 'Password must be at least 8 character long.';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'Password',
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.02,
                     ),
-                    obscureText: true,
-                    onSaved: (value) {
-                      _userPassword = value;
-                    },
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  if (widget.isLoading) CircularProgressIndicator(),
-                  if (!widget.isLoading)
-                    RaisedButton(
-                      child: Text(_isLogin ? 'Login' : 'SignUp'),
-                      onPressed: _trySubmit,
+                    if (widget.isLoading) CircularProgressIndicator(),
+                    if (!widget.isLoading)
+                      Center(
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          child: RaisedButton(
+                            color: Color(0xff3F70B5),
+                            child: Text(
+                              _isLogin ? 'Log In' : 'Sign Up',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            onPressed: _trySubmit,
+                          ),
+                        ),
+                      ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.025,
                     ),
-                  if (!widget.isLoading)
-                    FlatButton(
-                      textColor: Theme.of(context).primaryColor,
-                      child: Text(_isLogin
-                          ? 'Create new account'
-                          : 'I already have an account'),
-                      onPressed: () {
-                        setState(() {
-                          _isLogin = !_isLogin;
-                        });
-                      },
+                    Center(
+                      child: Text(
+                        _isLogin
+                            ? 'Don\'t have an account?'
+                            : 'Already have an account?',
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
                     ),
-                ],
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.015,
+                    ),
+                    if (!widget.isLoading)
+                      Center(
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.05,
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            border: Border.all(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          child: RaisedButton(
+                            elevation: 0,
+                            color: Colors.white,
+                            textColor: Colors.grey,
+                            child:
+                                Text(_isLogin ? 'Create account' : 'Log In?'),
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
